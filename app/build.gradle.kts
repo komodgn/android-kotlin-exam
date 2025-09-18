1c2eb977ee81a9865b038de836cd96acdcf06515
komodgn <a01075251881@gmail.com>
2025-09-18 15:47:24 +0900

    feat: AndroidApplicationConventionPlugin 적용
    - build-logic 모듈에 컨벤션 플러그인 등록
    - app 모듈에 컨벤션 플러그인 적용 및 중복 코드 제거
    - 플러그인 등록 시 id는 `libs.versions.toml` 파일과 동일하게 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,11 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.multi.module.android.application)
 }
 
 android {
-    namespace = "com.example.noti"
-    compileSdk = 36
+    namespace = "com.multi.module.template"
 
     defaultConfig {
-        applicationId = "com.example.noti"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
