e65a4af8728665b168f999fabe72fa602673334a
komodgn <a01075251881@gmail.com>
2025-09-23 22:48:31 +0900

    refactor: 애플리케이션 Compose 플러그인에 컴파일러 플러그인 추가
    - 이전 커밋과 동일한 이슈

--- build-logic/convention/src/main/java/AndroidApplicationComposeConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationComposeConventionPlugin.kt
@@ -1,4 +1,5 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.example.noti.convention.applyPlugins
 import com.example.noti.convention.configureAndroidCompose
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -6,8 +7,11 @@ import org.gradle.kotlin.dsl.getByType
 
 class AndroidApplicationComposeConventionPlugin: Plugin<Project> {
     override fun apply(target: Project) {
-        target.run {
-            pluginManager.apply("multi.module.android.application")
+        with(target) {
+            applyPlugins(
+                "multi.module.android.application",
+                "org.jetbrains.kotlin.plugin.compose"
+            )
 
             val extension = extensions.getByType<ApplicationExtension>()
             configureAndroidCompose(extension)
