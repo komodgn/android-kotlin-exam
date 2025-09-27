7421ac80896db4cbbf7728f034060dec1a4d75ec
komodgn <a01075251881@gmail.com>
2025-09-27 22:39:43 +0900

    feat: core/ui 모듈 생성 (#1)
    - 각 feature 모듈은 core/ui를 의존하도록 구현 (AndroidFeatureConventionPlugin에 의존성으로 core/ui 추가)

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.multi.module.android.library.compose)
+}
+
+android {
+    namespace = "com.example.noti.core.ui"
+}
+
+dependencies {
+    implementation(projects.core.designsystem)
+    implementation(projects.core.common)
+
+    implementation(libs.compose.keyboard.state)
+    implementation(libs.compose.effects)
+
+    implementation(libs.logger)
+}
