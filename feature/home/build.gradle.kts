17f896266f74ddab50d72d2f30d9b9150f0e6f57
komodgn <a01075251881@gmail.com>
2025-09-27 20:31:14 +0900

    feat: feature/home 모듈 생성 (#1)
    TODO:
    - 플러그인 주석 해제

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    alias(libs.plugins.multi.module.android.feature)
+    alias(libs.plugins.kotlin.serialization)
+//    alias(libs.plugins.kotlin.parcelize)
+}
+
+android {
+    namespace = "com.example.noti.feature.home"
+}
+
+//ksp {
+//    arg("circuit.codegen.mode", "hilt")
+//}
+
+dependencies {
+//    implementation(libs.lottie.compose)
+
+    implementation(libs.logger)
+}
