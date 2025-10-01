3e1de622680a469afe7891a4cc8e0eae206b6be7
komodgn <a01075251881@gmail.com>
2025-10-01 21:37:10 +0900

    feat: Onboarding, Login, Home 스크린 등록 (#13)
    - AnalyticsHelper가 스크린 이름으로 쉽게 로깅 하도록 추상 클래스(NotiScreen)를 선언하고 name 속성 추가 (로깅 메서드의 매개변수에 SplashScreen.name과 같이 전달하여 하드 코딩을 방지하기 위함)
    
    Parcelable 개념 참고 자료:
    - https://jinudmjournal.tistory.com/224

--- feature/screens/src/main/java/com/sample/noti/feature/screens/ScreenNames.kt
+++ feature/screens/src/main/java/com/sample/noti/feature/screens/ScreenNames.kt
@@ -2,4 +2,7 @@ package com.sample.noti.feature.screens
 
 object ScreenNames {
     const val SPLASH = "splash"
+    const val ONBOARDING = "onboarding"
+    const val LOGIN = "login"
+    const val HOME = "home"
 }
--- feature/screens/src/main/java/com/sample/noti/feature/screens/Screens.kt
+++ feature/screens/src/main/java/com/sample/noti/feature/screens/Screens.kt
@@ -1,8 +1,7 @@
 package com.sample.noti.feature.screens
 
 import com.slack.circuit.runtime.screen.Screen
-import kotlinx.android.parcel.Parcelize
-
+import kotlinx.parcelize.Parcelize
 
 abstract class NotiScreen(val name: String) : Screen {
     override fun toString(): String = name
@@ -10,3 +9,14 @@ abstract class NotiScreen(val name: String) : Screen {
 
 @Parcelize
 data object SplashScreen : NotiScreen(name = ScreenNames.SPLASH)
+
+//object SplashScreen : Screen, Parcelable
+
+@Parcelize
+data object OnboardingScreen : NotiScreen(name = ScreenNames.ONBOARDING)
+
+@Parcelize
+data object HomeScreen : NotiScreen(name = ScreenNames.HOME)
+
+@Parcelize
+data object LoginScreen : NotiScreen(name = ScreenNames.LOGIN)
