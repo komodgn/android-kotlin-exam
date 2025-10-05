a68f60cbdd8617fa7dcb8b64d1a409b9a1357c6b
komodgn <a01075251881@gmail.com>
2025-10-05 23:36:17 +0900

    feat: 서버 환경 변수 및 의존성 주입 초기 설정 (#17)
    - local.properties 파일에 서버 기본 주소 설정 (DEBUG_SERVER_BASE_URL, RELEASE_SERVER_BASE_URL)
    - 네트워크 모듈의 빌드 스크립트에서 환경 변수로 서버 기본 주소 추가
    - 네트워크 모듈에서 토큰 인터셉터 등을 구현하므로 datastore/api를 의존하도록 관계 설정
    - 네트워크 통신을 위한 클라이언트를 의존성 주입을 통해 생성하도록 구현

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     alias(libs.plugins.multi.module.android.library)
     alias(libs.plugins.multi.module.android.retrofit)
@@ -5,9 +7,29 @@ plugins {
 }
 
 android {
-    namespace = "com.example.noti.core.network"
+    namespace = "com.sample.noti.core.network"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    buildTypes {
+        debug {
+            buildConfigField("String", "SERVER_BASE_URL", getServerBaseUrl("DEBUG_SERVER_BASE_URL"))
+        }
+
+        release {
+            buildConfigField("String", "SERVER_BASE_URL", getServerBaseUrl("RELEASE_SERVER_BASE_URL"))
+        }
+    }
 }
 
 dependencies {
+    implementation(projects.core.datastore.api)
+
     implementation(libs.logger)
+}
+
+fun getServerBaseUrl(propertyKey: String): String {
+    return gradleLocalProperties(rootDir, providers).getProperty(propertyKey)
 }
