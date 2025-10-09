01e46355dcd9348b8e90173db0b7466d8c162eed
komodgn <a01075251881@gmail.com>
2025-10-09 20:43:53 +0900

    feat: Firebase Analytics 기능 추가 (#18)
    - FirebaseAnalytics 의존성 주입 설정
    - 헬퍼 클래스에 스크린 뷰 로깅을 위한 메서드 작성
    - 홈 화면에 적용

--- feature/home/src/main/java/com/sample/noti/feature/home/HomePresenter.kt
+++ feature/home/src/main/java/com/sample/noti/feature/home/HomePresenter.kt
@@ -2,11 +2,13 @@ package com.sample.noti.feature.home
 
 import android.util.Log
 import androidx.compose.runtime.Composable
+import com.sample.noti.core.common.analytics.AnalyticsHelper
 import com.sample.noti.feature.screens.HomeScreen
 import com.sample.noti.feature.screens.OcrScreen
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.presenter.Presenter
+import com.slack.circuitx.effects.ImpressionEffect
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
@@ -14,7 +16,7 @@ import dagger.hilt.android.components.ActivityRetainedComponent
 
 class HomePresenter @AssistedInject constructor(
     @Assisted private val navigator: Navigator,
-    // TODO: Add Analytics Helper
+    private val analyticsHelper: AnalyticsHelper
     // TODO: Add Repository
 ) : Presenter<HomeUiState> {
 
@@ -36,6 +38,10 @@ class HomePresenter @AssistedInject constructor(
                     navigator.goTo(OcrScreen)
                 }
             }
+        }
+
+        ImpressionEffect {
+            analyticsHelper.logScreenView(HomeScreen.name)
         }
 
         return HomeUiState(
