1d56c8032e7e432bfa75d7224dadf1affc1b012a
komodgn <a01075251881@gmail.com>
2025-10-11 18:55:40 +0900

    fix: 누락된 패키지 선언 추가

--- core/datastore/impl/src/main/java/com/sample/noti/core/datastore/impl/di/DataStoreModule.kt
+++ core/datastore/impl/src/main/java/com/sample/noti/core/datastore/impl/di/DataStoreModule.kt
@@ -1,10 +1,11 @@
+package com.sample.noti.core.datastore.impl.di
+
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import com.sample.noti.core.datastore.api.datasource.TokenDataSource
 import com.sample.noti.core.datastore.impl.datasource.TokenDataSourceImpl
-import com.sample.noti.core.datastore.impl.di.TokenDataStore
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
